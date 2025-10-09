9d787ccb346e6a748807e33ee4e99626d8c0bf9c
ismaiars <iarsfate@gmail.com>
2025-10-09 11:11:53 -0600

    añadido metricas vercel

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
 import App from "./App";
 import "./styles/tailwind.css";
 import "./styles/index.css";
+
+// Inyectar Speed Insights y Analytics
+injectSpeedInsights();
+inject();
 
 const container = document.getElementById("root");
 const root = createRoot(container);
